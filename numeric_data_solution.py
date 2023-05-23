5a46e52f4e456b3c294bd8323b822c59d4f446ff
LAmarilio <134454338+LAmarilio@users.noreply.github.com>
2023-05-23 20:44:28 -0300

    Add files via upload

---
+++ numeric_data_solution.py
@@ -0,0 +1,31 @@
+# Exercicio 1
+print("Python has three numeric types: int, float, and complex")
+
+# Exercicio 2
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercicio 3
+myValue = 3.14
+print(myValue)
+type(myValue)
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercicio 4
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercicio 5
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue = False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
---
+++ string_data_type_solution.py
@@ -0,0 +1,20 @@
+# Exercicio 1
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+# Exercicio 2
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+# Exercicio 3
+name = input("What is your name?")
+print(name)
+
+# Exercicio 4
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
